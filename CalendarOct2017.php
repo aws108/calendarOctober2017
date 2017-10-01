71318d3179e20b2a2a7c6cae96ef0d8131e56c1c
aws108 <slizanagarcia@iesesteveterradas.cat>
2017-10-01 20:21:07 +0200

    Update CalendarOct2017.php

--- CalendarOct2017.php
+++ CalendarOct2017.php
@@ -13,14 +13,16 @@
 
 <?php 
 $semanaSiguiente = time() + (7 * 24 * 60 * 60); // 7 días; 24 horas; 60 minutos; 60 segundos
+
+
 $mes = date("m",mktime(0,0,0,10,1,2017)); //echo date("M-d-Y", mktime(0, 0, 0, 1, 1, 1998));
 $anyo = date ("Y",mktime(0,0,0,10,1,2017));
 $semana = array ("Mon","Tue", "Wed", "Thu","Fri","Sat","Sun");
 echo "<table>";
     echo "<th>";
-        echo "<tr>";
+        //echo "<tr>";
             echo "<th colspan='7'>".date("M, Y",mktime(0,0,0,10,1,2017))."</th>"; //Mes de Octubre
-        echo "</tr>";
+        //echo "</tr>";
         echo "<tr>";
             echo "<th>Lunes</th><th>Martes</th><th>Miércoles</th><th>Jueves</th><th>Viernes</th><th>Sábado</th><th>Domingo</th>"; 
             //Días de la semana
@@ -36,17 +38,17 @@ echo "<table>";
                 //Si el día de la semana se corresponde con los días del array, píntame los días en formato numérico
                     echo "<td>". date("d", mktime(0,0,0,10,1,2017)) ."</td>";
 
-                    if (date("D",mktime(0,0,0,10,1,2017))=="Sun" ){ //Si el día de la semana se corresponde con "Domingo"
-                        echo "<td>". "</td>";
+                    /*if (date("D",mktime(0,0,0,10,1,2017))=="Sun" ){ //Si el día de la semana se corresponde con "Domingo"
+                        //echo "<td>". "</td>";
                         break;
                     }else{
                         break;
                     }
 
-                    break;        
+                    break;  */      
                 }
                 else{
-                    echo  "<td>". "</td>"  ;
+                    echo  "<td>". "</td>"  ; //Deja en blanco 6 casillas que corresponden con el "Sun"
                     }
                 }
     echo "</tr>";
